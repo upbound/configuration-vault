bf9ca5e2a2f0972568c919995f822628c35ff299
humoflife <markus.schweig@upbound.io>
2024-01-25 13:37:39 -0800

    update for git issue #3

--- test/setup.sh
+++ test/setup.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 # Uptest setup
 SCRIPT_DIR=$( cd -- $( dirname -- "${BASH_SOURCE[0]}" ) &> /dev/null && pwd )
+${KUBECTL} wait configuration.pkg configuration-vault --for=condition=Healthy --timeout 5m
+${KUBECTL} wait configuration.pkg configuration-vault --for=condition=Installed --timeout 5m
+${KUBECTL} wait configurationrevisions.pkg --all --for=condition=Healthy --timeout 5m
+${KUBECTL} wait xrd --all --for condition=Established
+
 ${KUBECTL} -n upbound-system wait --timeout=5m --for=condition=Available deployment --all
 
 ${KUBECTL} wait function.pkg --all --timeout 5m --for condition=Healthy
@@ -10,14 +15,8 @@ ${KUBECTL} apply -f ${SCRIPT_DIR}/provider/provider-configs.yaml
 SA=$(${KUBECTL} -n upbound-system get sa -o name|grep provider-kubernetes|\
    sed -e "s|serviceaccount\/|upbound-system:|g")
 ${KUBECTL} create clusterrolebinding provider-kubernetes-admin-binding \
-    --clusterrole cluster-admin --serviceaccount="${SA}"
+    --clusterrole cluster-admin --serviceaccount="${SA}" || true
 SA=$(${KUBECTL} -n upbound-system get sa -o name|grep provider-helm|\
    sed -e "s|serviceaccount\/|upbound-system:|g")
 ${KUBECTL} create clusterrolebinding provider-helm-admin-binding \
-    --clusterrole cluster-admin --serviceaccount="${SA}"
-
-find ${SCRIPT_DIR}/../apis -name "definition.yaml"|\
-    while read y; do ${KUBECTL} apply -f $y; done
-find ${SCRIPT_DIR}/../apis -name "composition.yaml"|\
-    while read y; do ${KUBECTL} apply -f $y; done
-#${KUBECTL} apply -f ${SCRIPT_DIR}/../examples/vault.yaml
+    --clusterrole cluster-admin --serviceaccount="${SA}" || true
